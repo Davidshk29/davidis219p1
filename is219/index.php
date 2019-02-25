c3a0c42e945c3f7d2015b4ef19f1e859e4e5f017
SeongK29 <35498245+Davidshk29@users.noreply.github.com>
2019-02-25 13:20:58 -0500

    Add files via upload

---
+++ is219/index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>David Kim</title>
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+</style>
+
+</head>
+
+<body class="w3-light-grey w3-content" style="max-width:1600px">
+
+    <!-- Navigation -->
+	<nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
+		<div class="w3-container">
+			<a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-jumbo w3-padding w3-hover-grey" title="close menu">
+			<i class="fa fa-remove"></i>
+			</a>
+			<img src="img/davidtopleft.jpg" style="width:70%;" class="w3-round"><br><br>
+			<h4><b>David Kim</b></h4>
+			<p class="w3-text-grey">NJIT</p>
+			<p class="w3-text-grey">Information Technology</p>
+		</div>
+		<div class="w3-bar-block">
+			<a href="#aboutme" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>About Me</a> 
+			<a href="#resume" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>Resume</a>
+			<a href="#skills" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>Skills</a> 
+			<a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw w3-margin-right"></i>Contact</a>
+		</div>
+		<div class="w3-panel w3-large">
+			<a href="https://www.instagram.com/daaavid_kim/?hl=en"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+			<a href="https://njit.joinhandshake.com/users/13259802"><i style="font-size:24px" class="fa">&#xf2b5;</i> </a>
+		</div>
+	</nav>
+	<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+    <!-- Page Content -->
+	<div class="w3-main" style="margin-left:300px">
+<header id="aboutme">
+    <a href="#"><img src="img/davidtopleft.jpg" style="width:65px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
+    <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
+    <div class="w3-container">
+    <h1><b>About Me</b></h1>
+	</div>
+  </header>
+  
+  <!-- First Photo Grid-->
+  <div class="w3-row-padding">
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="img/njit.png" alt="School" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>College: NJIT</b></p>
+        <p>Year: Senior</p>
+		<p>Major: Information Technology</p>
+      </div>
+    </div>
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="img/flag.jpg" alt="Born" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>From: South Korean</b></p>
+        <p>Born in: Seoul</p>
+		<p>Date of birth: 10/27/1996</p>
+      </div>
+    </div>
+    <div class="w3-third w3-container">
+      <img src="img/snowboarding.jpg" alt="Funfact" style="width:88.5%" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p><b>Hobby</b></p>
+		<p>Snowboard</p>
+		<p>Longboard</p>
+        <p>Games</p>
+      </div>
+    </div>
+  </div>
+  <!-- Resume -->
+   <div class="w3-container w3-white" style="margin-bottom:32px" id="resume">
+    <h4><b>Resume</b></h4>
+    <p><b>Work Experience</b></p>
+	<p>NJIT ITMS</p>
+	<p>Event Staff/ Tech Support</p>
+	<p>September 2017-Present</p>
+	<ul>
+		<li>Help people with technology troubles</li>
+		<li>Troubleshoot, setup events, and fixing computers/projectors/rewiring cables.</li>
+		<li>Updated and installed software</li>
+		<li>Replaced outdated and malfunctioning hardware and equipment.</li>
+	</ul>
+	<div align="center">
+	<a href="https://ist.njit.edu/usage-policy-njit-community/">
+		<img src="img/njit.png" alt="njit" style="width:10%">
+		</br>
+	</a>
+	</div>
+	<p><b>Organiztion</b></p>
+	<p>Kappa Xi Kappa</p>
+	<p>Fundraiser/Risk Management/House Management</p>
+	<p>January 2017-Present</p>
+	<ul>
+		<li>Guiding Fraternity brothers to do the right thing and helping them get certification for CPR.</li>
+		<li>Organized plans to fix the house or any repairs that needs to be done around the house with the brothers</li>
+		<li>Assisted the President with the oversight of the student organization including fundraising and event planning</li>
+		<li>Installed cameras for the house for security.</li>
+	</ul>
+	<p>
+	<div align="center">
+	<a href="http://www.kappaxikappa.com/">
+	<img src="img/logo.png" alt="kappa" style="width:10%">
+	</br>
+	</a>
+	</div>
+	<h1 align="center">Philantrophy Involved</h1>
+			<h2 align="center">Fall</h2>
+			<p>Movember</p>
+			As many might know, the Movember Foundation started the trend of “No Shave November” to bring
+			awareness to men’s health. The origin of the word Movember dates back to a group in South Australia 
+			growing out their moustaches for charity. Grabbing airtime on a local news network, the movement 
+			started from a group of about 80 men to a national phenomenon. Several years later, an unrelated 
+			group in the Australian state of Victoria where 30 men would grow out their facial hair for 30 days 
+			to raise awareness. This second group of 30 men would later become the Movember Foundation Charity. 
+			Since its inception, the Movember foundation has raised nearly 200 million worldwide for the purposes
+			of helping enrich and enhance men’s health. While originally just for prostate cancer and mental health
+			in men, The Movember Foundation absorbed the testicular cancer fundraiser Tacheback. We at Kappa Xi
+			Kappa are proud to say that we have supported Movember evey fall for the past seven years. In the fall
+			2018 semester we raised over 2,000 dollars for men’s health with our campaigns of tabling on Wednesdays
+			and or Annual Movember Gala Dinner. 
+			<br>
+			<div align="center">
+			<a href="https://us.movember.com/team/2331296">
+			<img src="BANNER1.gif" alt="Movember" style="width:50%" align="middle">
+			</a>
+			</div>
+		</br>	
+			<h2 align="center">Spring</h2>
+			<p>Camp Moore</p>
+			Many children go to summer camp every year and don’t realize how important it is for a child’s psyche
+			and social development. Many of us have some of funniest memories from summer camp or a friend we would
+			not have met had it not been for that camp.  The Elks association, particularly those in New Jersey,
+			have pooled together resources to create a safe and inclusive weeklong summer camp for children with 
+			special needs: Camp Moore.
+			Unbeknownst to many, this would be the second iteration of Camp Moore. The first Camp Moore, which was 
+			named after the former Governor, was established in the 1930s under similar premises as its contemporary
+			but was close to shutting down due to financial difficulties. The Elks were, at the time, searching for
+			camp. One thing lead to another and the current iteration of the camp was born in the 1970s and it has
+			been running every summer since. One our brothers is part of an Elk’s Lodge and offered the active
+			brotherhood a chance to support this amazing camp several years ago. Without second thought, Kappa Xi 
+			Kappa has been supporting the camp every since with our weekly informational events and dinner. 
+
+			<br>
+			<div align="center">
+			<a href="https://www.elks.org/enf/programs/StateProject.cfm?vhpID=8028">
+			<img src="BANNER2.jpg" alt="campmoore" style="width:50%" >
+			</a>
+			</div>
+	  </p>
+	
+	</div>
+	<div class="w3-container w3-white" style="margin-bottom:32px" id="skills">
+    <h4><b>Skills</b></h4>
+    <p><b>Technical</b></p>
+	<ul>
+		<li>Java, Python, HTML, C++, Php,MySQL, SQL, CSS, and Microsoft Suite</li>
+		<li>XML/AJAX</li>
+		<li>Wordpress</li>
+		<li>Access</li>
+	</ul>
+	<p>Skilled Technician who has coding background. Enjoys troubleshooting to find solutions for technical issues and resolving them in given time. Skilled developing websites with client/server applications. </p>
+	<p><b>Proficient in Korean</b></p>
+	
+	</div>
+        <!-- /.row -->
+		  <!-- Contact Section -->
+  <div class="w3-container w3-padding-large w3-grey">
+    <h4 id="contact"><b>Contact Me</b></h4>
+    <div class="w3-row-padding w3-center w3-padding-24" style="margin:0 -16px">
+      <div class="w3-third w3-dark-grey">
+        <p><i class="fa fa-envelope w3-xxlarge w3-text-light-grey"></i></p>
+        <p>shk29@njit.edu</p>
+      </div>
+      <div class="w3-third w3-teal">
+        <p><i class="fa fa-map-marker w3-xxlarge w3-text-light-grey"></i></p>
+        <p>405 Park Ave, Rutherford, NJ 07070</p>
+      </div>
+      <div class="w3-third w3-dark-grey">
+        <p><i class="fa fa-phone w3-xxlarge w3-text-light-grey"></i></p>
+        <p>(201)-245-8202</p>
+      </div>
+    </div>
+  </div>
+
+
+		<!-- Footer -->
+  <footer class="w3-container w3-padding-32 w3-dark-grey">
+  <div class="w3-row-padding">
+    <div class="w3-third">
+      <h3>View Counter </h3>
+	  <div><script type="text/javascript" src="http://services.webestools.com/cpt_pages_views/65174-8-5.js"></script></div>
+	  <p>Created by David Kim and the help of w3school</p>
+	</div>
+  </footer>
+    <!-- /.container -->
+
+    <!-- jQuery Version 3.3.1 -->
+<script>
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+</script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
